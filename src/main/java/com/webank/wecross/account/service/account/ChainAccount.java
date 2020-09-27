3883e691096edc540b4adf3afd80d59d7cd1584a
JimmyShi22 <417711026@qq.com>
2020-09-27 18:56:53 +0800

    return ext

--- src/main/java/com/webank/wecross/account/service/account/ChainAccount.java
+++ src/main/java/com/webank/wecross/account/service/account/ChainAccount.java
@@ -88,6 +88,7 @@ public class ChainAccount {
         this.secKey = secKey;
     }
 
+    @JsonGetter("ext")
     public String getExt0() {
         return ext0;
     }
@@ -96,24 +97,12 @@ public class ChainAccount {
         this.ext0 = ext0;
     }
 
-    public String getExt1() {
-        return ext1;
-    }
-
     public void setExt1(String ext1) {
         this.ext1 = ext1;
     }
 
-    public String getExt2() {
-        return ext2;
-    }
-
     public void setExt2(String ext2) {
         this.ext2 = ext2;
-    }
-
-    public String getExt3() {
-        return ext3;
     }
 
     public void setExt3(String ext3) {
