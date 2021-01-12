c0483feb6ff50a101034dac26980baa56a16c090
octopus <912554887@qq.com>
2021-01-12 11:19:16 +0800

    add wecross.security file

--- scripts/start.sh
+++ scripts/start.sh
@@ -10,6 +10,8 @@ STATUS_STARTING="Starting"
 STATUS_RUNNING="Running"
 STATUS_STOPPED="Stopped"
 
+SECURIY_FILE='./.wecross.security'
+
 LOG_INFO()
 {
     local content=${1}
@@ -22,6 +24,14 @@ LOG_ERROR()
     echo -e "\033[31m${content}\033[0m"
 }
 
+create_jvm_security()
+{
+  if [[ ! -f ${SECURIY_FILE} ]];then
+    echo "jdk.disabled.namedCurves = " > ${SECURIY_FILE}
+    # LOG_INFO "create new file ${SECURIY_FILE}"
+  fi
+}
+
 wecross_pid()
 {
     ps -ef | grep ${APP_NAME} | grep ${APPS_FOLDER} | grep -v grep | awk '{print $2}'
@@ -31,13 +41,13 @@ run_wecross()
 {
     if [ "$(uname)" == "Darwin" ]; then
         # Mac
-        nohup java -Djdk.disabled.namedCurves="" -Djdk.sunec.disableNative="false" -Djdk.tls.namedGroups="secp256k1" -cp ${CLASS_PATH} ${APP_NAME} >start.out 2>&1 &
+        nohup java -Djava.security.properties=${SECURIY_FILE} -Djdk.sunec.disableNative="false" -Djdk.tls.namedGroups="secp256k1" -cp ${CLASS_PATH} ${APP_NAME} >start.out 2>&1 &
     elif [ "$(uname -s | grep MINGW | wc -l)" != "0" ]; then
         # Windows
-        nohup java -Djdk.disabled.namedCurves="" -Djdk.sunec.disableNative="false" -Djdk.tls.namedGroups="secp256k1" -cp ${WINDS_CLASS_PATH} ${APP_NAME} >start.out 2>&1 &
+        nohup java -Djava.security.properties=${SECURIY_FILE} -Djdk.sunec.disableNative="false" -Djdk.tls.namedGroups="secp256k1" -cp ${WINDS_CLASS_PATH} ${APP_NAME} >start.out 2>&1 &
     else
         # GNU/Linux
-        nohup java -Djdk.disabled.namedCurves="" -Djdk.sunec.disableNative="false" -Djdk.tls.namedGroups="secp256k1" -cp ${CLASS_PATH} ${APP_NAME} >start.out 2>&1 &
+        nohup java -Djava.security.properties=${SECURIY_FILE} -Djdk.sunec.disableNative="false" -Djdk.tls.namedGroups="secp256k1" -cp ${CLASS_PATH} ${APP_NAME} >start.out 2>&1 &
     fi
 }
 
@@ -89,6 +99,7 @@ before_start()
 start()
 {
     rm -f start.out
+    create_jvm_security
     run_wecross
     echo -e "\033[32mWeCross-Account-Manager booting up ..\033[0m\c"
     try_times=45
